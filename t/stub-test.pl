4205e19c58ffa1dfc9e68bf801c78e8806440dc4
Giovanni Bechis <g.bechis@snb.it>
2022-06-28 16:19:33 +0200

    add wip tests

---
+++ t/stub-test.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl -w 
+
+BEGIN {
+  unshift (@INC, "lib");
+}
+
+use Net::validMX;
+use strict;
+
+print "DEBUG: Net::validMX::version: ".&Net::validMX::version()."\n\n";
+
+my ($rv, $reason, $valid);
+
+#OO METHOD - Set any settings you want here
+$valid = Net::validMX->new(allow_ip_address_as_mx=>0, debug=>1);
+
+print "OO\n";
+
+#FEEL FREE TO OVERRIDE SETTINGS PER FUNCTION AS WELL
+($rv, $reason) = $valid->check_valid_mx(email=>'kevin@mcgrail.com', debug=>1, allow_ip_address_as_mx=>0);
+
+print "DEBUG: rv/reason: $rv - $reason\n";
+
+#PROCEDURAL METHOD
+
+print "\nProdecural\n";
+
+$Net::validMX::ALLOW_IP_ADDRESS_AS_MX = 0;
+$Net::validMX::DEBUG = 0;
+
+($rv, $reason) = &Net::validMX::check_valid_mx(email=>'kevin@mcgrail.com', debug=>1, allow_ip_address_as_mx=>1);
+
+print "DEBUG: rv/reason: $rv - $reason\n";
+
+($rv, $reason) = &Net::validMX::check_valid_mx('kevin@mcgrail.com', debug=>0);
+
+print "DEBUG: rv/reason: $rv - $reason\n";
+
+#allow_ip_address_as_mx
