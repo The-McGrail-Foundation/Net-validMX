78ddc2e004d6015de9e0b58269842b9871f94cd4
Giovanni Bechis <giovanni@paclan.it>
2023-04-04 10:50:17 +0200

    Release 2.5.1

--- lib/Net/validMX.pm
+++ lib/Net/validMX.pm
@@ -1011,7 +1011,7 @@ kevin.mcgrail-netvalidmx@mcgrail.com
 
 =item v2.5  Released May 8, 2020.  Numerous bug fixes and moved to The McGrail Foundation.  This product is in active use in production system.
 
-=item v2.5.1  In development.  Small documentation bug fixes.
+=item v2.5.1 Released April 4, 2023. Documentation bug fixes and ipv6 improvements.
 
 =back
 
